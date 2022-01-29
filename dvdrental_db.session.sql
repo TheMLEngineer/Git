5a853fb2000ef05c0d6fdc0c7f42b6faabda8029
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-01-29 16:05:52 +0530

    M

--- dvdrental_db.session.sql
+++ dvdrental_db.session.sql
@@ -1,1 +1,6 @@
-SELECT * FROM actor;
+-- SELECT * FROM customer
+-- UNION
+SELECT address.address_id , address.district FROM address
+FULL OUTER JOIN customer
+ON address.address_id = customer.customer_id
+ 
