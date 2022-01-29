f5b733e4556eb061c4aa4575e295884591ea760e
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-01-29 22:11:55 +0530

    M

---
+++ psql_db.session.sql
@@ -0,0 +1,18 @@
+
+
+CREATE TABLE account(
+    user_id SERIAL PRIMARY KEY ,
+    username VARCHAR(50) UNIQUE NOT NULL ,
+    password VARCHAR(50) NOT NULL ,
+    email VARCHAR(250) UNIQUE NOT NULL ,
+    created_on TIMESTAMP NOT NULL ,
+    last_login TIMESTAMP 
+)
+
+
+
+
+CREATE TABLE job(
+    job_id SERIAL PRIMARY KEY ,
+    job_name VARCHAR(500) UNIQUE NOT NULL
+)
