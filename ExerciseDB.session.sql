a13d54a7407d5fcd7634306aae3a4f343c8a949a
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-01-29 19:32:05 +0530

    M

--- ExerciseDB.session.sql
+++ ExerciseDB.session.sql
@@ -1,10 +1,16 @@
 
 
 
-
-
-
 /*
+
+
+
+SELECT * FROM cd.bookings
+INNER JOIN cd.members
+ON cd.bookings.memid = cd.members.memid
+WHERE firstname = 'David'
+AND surname = 'Farrell'
+
 
 
 SELECT starttime , name FROM
