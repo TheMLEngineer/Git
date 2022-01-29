6f36d6129f4ceade25372384dccf98a3fbf8839f
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-01-29 18:43:14 +0530

    M

--- ExerciseDB.session.sql
+++ ExerciseDB.session.sql
@@ -1,11 +1,21 @@
 
 
-SELECT DISTINCT(surname) FROM cd.members
-ORDER BY surname
-LIMIT 10
+
+
+
 
 
 /*
+
+SELECT COUNT(*) FROM cd.facilities
+WHERE guestcost >= 10
+
+SELECT MAX(joindate) FROM cd.members
+
+
+SELECT DISTINCT(surname) FROM cd.members
+ORDER BY surname
+LIMIT 10
 SELECT * FROM cd.members
 WHERE joindate >= '2012-09-01'
 
