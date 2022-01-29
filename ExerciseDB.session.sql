09b5cb32a76e962cbd6a89e207f5a99a406d5736
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-01-29 19:12:19 +0530

    M

--- ExerciseDB.session.sql
+++ ExerciseDB.session.sql
@@ -4,8 +4,25 @@
 
 
 
-
 /*
+
+
+SELECT * FROM 
+
+(SELECT facid , SUM(slots) as total FROM cd.bookings
+GROUP BY facid
+ORDER BY facid) AS subquery
+WHERE subquery.total > 1000
+
+
+
+SELECT cd.bookings.facid , SUM(slots) FROM cd.bookings
+INNER JOIN cd.facilities 
+ON cd.bookings.facid = cd.facilities.facid
+WHERE starttime >= '2012-09-01'
+GROUP BY cd.bookings.facid , cd.bookings.bookid , cd.facilities.facid
+
+
 
 SELECT COUNT(*) FROM cd.facilities
 WHERE guestcost >= 10
