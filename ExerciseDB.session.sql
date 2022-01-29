4dfdbd03105b2b8472a6b4b33c409075d51decc0
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-01-29 18:38:17 +0530

    M

---
+++ ExerciseDB.session.sql
@@ -0,0 +1,16 @@
+
+
+SELECT DISTINCT(surname) FROM cd.members
+ORDER BY surname
+LIMIT 10
+
+
+/*
+SELECT * FROM cd.members
+WHERE joindate >= '2012-09-01'
+
+SELECT * FROM cd.facilities
+WHERE facid = 1 OR facid = 5
+
+
+*/
