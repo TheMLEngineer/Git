788e07dbd18eb13027466d1915c944fa392b1202
Karthikeyan Muthu <phenomenalkarthikeyan@gmail.com>
2022-02-06 15:39:43 +0530

    M

--- psql_db.session.sql
+++ psql_db.session.sql
@@ -2,9 +2,27 @@
 
 
 
+
+
 /*
 
 
+
+UPDATE account
+SET last_login = CURRENT_TIMESTAMP
+RETURNING email , created_on , last_login
+
+SELECT * FROM account
+
+
+UPDATE account_job
+SET hire_date = account.created_on
+FROM account
+WHERE account_job.user_id = account.user_id
+
+
+UPDATE account
+SET last_login = CURRENT_TIMESTAMP
 
 SELECT * FROM account_job
 
